31bb605d14458f2713f2d8e2184c99d41d615d66
BT_2S14_34 <leo1281515@gmail.com>
2024-09-17 11:02:27 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //converts to decimal format
+        DecimalFormat currency= new DecimalFormat("$ #,##0.00");
+
+        //obtain bill by user input
+        System.out.println("Please enter the bill in dollars and cents: ");
+        double bill = scan.nextDouble();
+        //obtain tip amount by user input
+        System.out.println("What is the tip percentage: ");
+        double tip = scan.nextDouble() / 100;
+        double totalTip = bill * tip;
+        //obtain amount of people by user input
+        System.out.println("Number of people: ");
+        int totalPeople = scan.nextInt();
+
+        //calculate the amount each person has to pay
+        double tipPer = totalTip / totalPeople;
+        double totalBill = totalTip + bill;
+        double billPer = totalBill / totalPeople;
+
+
+        //convert bill into dollar and cents form
+        int billInt = (int) (billPer);
+        double decimal = billPer - billInt;
+        int centsInt = (int)((decimal * 100) + 0.50);
+        double centConvert = (double) centsInt / 100;
+        double lastBillPer = billInt + centConvert;
+
+        //convert tip into dollar and cents form
+        int tipInt = (int) (tipPer);
+        double decimalTip = tipPer - tipInt;
+        int tipCentsInt = (int)((decimalTip * 100) + 0.50);
+
+        //test
+        System.out.println(tipCentsInt);
+
+        double centConvert2 = (double) tipCentsInt / 100;
+
+        //test
+        System.out.println(centConvert2);
+
+        double lastTipPer = tipInt + centConvert2;
+
+        System.out.println("Tip per person: $" + lastTipPer);
+        System.out.println("Bill per person: $" +  lastBillPer);
+
+
+
+    }
+}
